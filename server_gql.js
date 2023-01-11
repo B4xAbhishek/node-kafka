92670e21ed2cf029cc59715f120d240f94d3e9c1
Abhishek Kumar Verma <50478316+B4xAbhishek@users.noreply.github.com>
2023-01-11 22:30:30 +0530

    Update server_gql.js

--- server_gql.js
+++ server_gql.js
@@ -4,6 +4,55 @@ const { typeDefs } = require( "./schema/type-defs");
 const { resolvers } = require( "./schema/resolvers");
 const server = new ApolloServer({ typeDefs, resolvers });
 
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const UserProfile = ({ id }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      setError(null);
+  
+      try {
+        const query = `
+          query GetUser($id: ID!) {
+            user(id: $id) {
+              name
+              email
+            }
+          }
+        `;
+        const { data } = await axios.post('/graphql', {
+          query,
+          variables: { id },
+        });
+        setUser(data.data.user);
+      } catch (err) {
+        setError(err);
+      }
+  
+      setLoading(false);
+    };
+  
+    fetchData();
+  }, [id]);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error.message}</p>;
+
+  return (
+    <div>
+      <p>Name: {user.name}</p>
+      <p>Email: {user.email}</p>
+    </div>
+  );
+};
+
+export default UserProfile;
 
 server.listen().then(({url}) => {
     console.log("YOUR API IS RUNNING :",url)
